114abe17360b819bd1d6cdb203905b7bd9f224ea
hector <hpulido1021@gmail.com>
2024-01-20 15:21:27 -0500

    Feature: created next.js app leetcode, added navbar, login, signup and restore password

---
+++ src/atoms/authModalAtom.ts
@@ -0,0 +1,17 @@
+import { atom } from "recoil";
+
+type AuthModalState = {
+	isOpen: boolean;
+	type: "login" | "register" | "forgotPassword";
+};
+
+const initalAuthModalState: AuthModalState = {
+	isOpen: false,
+	type: "login",
+};
+
+ const authModalState = atom<AuthModalState>({
+	key: "authModalState",
+	default: initalAuthModalState,
+});
+export default authModalState;
