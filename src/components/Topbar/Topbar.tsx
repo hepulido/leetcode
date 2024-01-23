dc7237ce34508cc8be0b12039b6f793b3adacc42
hector <hpulido1021@gmail.com>
2024-01-22 22:28:07 -0500

    Feature: added logout and reset password funtionality

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
@@ -6,6 +7,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useSetRecoilState } from "recoil";
 import  authModalState  from "@/atoms/authModalAtom";
 import { auth } from "@/firebase/firebase";
+import Logout from '../Buttons/Logout';
 
 type TopbarProps = {
     problemPage?: boolean;
@@ -14,7 +16,7 @@ type TopbarProps = {
 const Topbar:React.FC<TopbarProps> = ({ problemPage }) => {
     const [user] = useAuthState(auth);
 	const setAuthModalState = useSetRecoilState(authModalState);
-	const router = useRouter();
+	// const router = useRouter();
     
     return (
 		<nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
@@ -23,31 +25,7 @@ const Topbar:React.FC<TopbarProps> = ({ problemPage }) => {
 					<Image src='/logo-full.png' alt='Logo' height={100} width={100} />
 				</Link>
 
-				{problemPage && (
-					<div className='flex items-center gap-4 flex-1 justify-center'>
-						<div
-							className='flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer'
-							// onClick={() => handleProblemChange(false)}
-						>
-							{/* {/* <FaChevronLeft /> */}
-						</div> 
-						<Link
-							href='/'
-							className='flex items-center gap-2 font-medium max-w-[170px] text-dark-gray-8 cursor-pointer'
-						>
-							<div>
-								{/* <BsList /> */}
-							</div>
-							<p>Problem List</p>
-						</Link>
-						<div
-							className='flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer'
-							// onClick={() => handleProblemChange(true)}
-						>
-							{/* {/* <FaChevronRight /> */}
-						</div> 
-					</div>
-				)}
+			
 
 				<div className='flex items-center space-x-4 flex-1 justify-end'>
 					<div>
@@ -81,7 +59,7 @@ const Topbar:React.FC<TopbarProps> = ({ problemPage }) => {
 							</div>
 						</div>
 					)}
-					{user }
+					{user  && <Logout />}
 				</div>
 			</div>
 		</nav>
